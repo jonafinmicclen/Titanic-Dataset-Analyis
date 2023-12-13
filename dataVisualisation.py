ca6a7e64203ee8ae3fcd7e6d475c243684df9431
Jonathan McClen <jonathangmcclen@gmail.com>
2023-12-13 05:58:45 +0000

    Fixed histogram not aligned

--- dataVisualisation.py
+++ dataVisualisation.py
@@ -6,64 +6,61 @@ from phik import phik_matrix
 import LogisticRegression as lr
 
 #To disable sections of code
-ANALYSE_MODE = False
-REGRESSION_MODE = True
+ANALYSE_MODE = True
+REGRESSION_MODE = False
 
 #Load csv into a pandas dataframe
-titanicCSV = pd.read_csv('./Titanic.csv')
+titanic_dataset = pd.read_csv('./Titanic.csv')
 
 #Remove unwanted columns
-titanicCSV = titanicCSV.drop(["Cabin", "Name", "Ticket"], axis=1)
+titanic_dataset = titanic_dataset.drop(["Cabin", "Name", "Ticket"], axis=1)
 
 #Remove rows with null values (null age values)
-titanicCSV = titanicCSV.dropna()
-
-#Create new seperate dataframes of only Survived and Non Survived passengers
-titanicCSV_SURVIVED_True = titanicCSV[titanicCSV['Survived']==1]
-titanicCSV_SURVIVED_False = titanicCSV[titanicCSV['Survived']==0]
+titanic_dataset = titanic_dataset.dropna()
 
 #One hot encode binary data
-titanicCSV = pd.get_dummies(titanicCSV, columns=['Sex'])
-titanicCSV = pd.get_dummies(titanicCSV, columns=['Pclass'])
-titanicCSV = pd.get_dummies(titanicCSV, columns=['Embarked'])
+titanic_dataset = pd.get_dummies(titanic_dataset, columns=['Sex'])
+titanic_dataset = pd.get_dummies(titanic_dataset, columns=['Pclass'])
+titanic_dataset = pd.get_dummies(titanic_dataset, columns=['Embarked'])
 
 #Put analytical functions seperate so will not have to run every time
 if ANALYSE_MODE:
 
-    #Show data is correctly setup
-    print(titanicCSV)
+    #Show data has correctly loaded setup
+    print(titanic_dataset)
 
     #Initial analyis of data using built in describe function
-    print(titanicCSV.describe(include='all'))
+    print(titanic_dataset.describe(include='all'))
 
     #Show corelation values in relation to all variables and survived
-    print(phik_matrix(titanicCSV, interval_cols=['PassengerId', 'Age', 'SibSp', 'Fare'])['Survived'])
-
-    #Find frequency of survival for age ranges, then divide by frequency of all ages to remove offset
-    num_of_bins = 20
-    #Calculate Y(frequency) values for histogram of all ages
-    ageCounts, binEdge = np.histogram(titanicCSV['Age'], bins = num_of_bins)
-    #Calculate Y(frequency) values for histogram of ages of only the survivors
-    ageCountsAlive, _ = np.histogram(titanicCSV_SURVIVED_True['Age'], bins = binEdge)
-    #Plot the frequencys of the survivors divided by frquencys of all passengers to remove bias from the initial distribution of ages
-    plt.bar(np.linspace(0,80, num_of_bins), ageCountsAlive/ageCounts, width=np.diff(binEdge))
+    print(phik_matrix(titanic_dataset, interval_cols=['PassengerId', 'Age', 'SibSp', 'Fare'])['Survived'])
+    
+    #Create new seperate dataframes of only Survived and Non Survived passengers
+    titanic_dataset_SURVIVED_True = titanic_dataset[titanic_dataset['Survived']==1]
+    titanic_dataset_SURVIVED_False = titanic_dataset[titanic_dataset['Survived']==0]
+    #Plot the frequencys of the survivors divided by frequencys of all passengers to remove bias from the initial distribution of ages
+    num_of_bins = 50
+    ageCountsTotal, binEdge = np.histogram(titanic_dataset['Age'], bins = num_of_bins)
+    binWidth = np.diff(binEdge)
+    ageCountsSurvived, _ = np.histogram(titanic_dataset_SURVIVED_True['Age'], bins = binEdge)
+    plt.bar(np.linspace(binEdge[0],binEdge[-2], num_of_bins)+binWidth/2, ageCountsSurvived/ageCountsTotal, width=binWidth)
     plt.title('Age')
     plt.show()
 
 if REGRESSION_MODE:
 
     #Normalise continuous data
-    titanicCSV['Age']= titanicCSV['Age']/titanicCSV['Age'].max()
-    titanicCSV['Fare']= titanicCSV['Fare']/titanicCSV['Fare'].max()
+    titanic_dataset['Age']= titanic_dataset['Age']/titanic_dataset['Age'].max()
+    titanic_dataset['Fare']= titanic_dataset['Fare']/titanic_dataset['Fare'].max()
 
     #Create new column for has family based on parch and sibsp
-    titanicCSV['HasFamily']= (titanicCSV['Parch'] != 0) | (titanicCSV['SibSp'] != 0)
+    titanic_dataset['HasFamily']= (titanic_dataset['Parch'] != 0) | (titanic_dataset['SibSp'] != 0)
 
     #Create matrix of features/input only then add a all ones column for the bias weight, b0
-    featureMatrix = np.matrix(titanicCSV.drop(["Survived", 'PassengerId', 'Parch', 'SibSp'], axis=1).values).astype(float)
+    featureMatrix = np.matrix(titanic_dataset.drop(["Survived", 'PassengerId', 'Parch', 'SibSp'], axis=1).values).astype(float)
     featureMatrix = np.c_[featureMatrix, np.ones((featureMatrix.shape[0], 1))]
     #Create vector of actual outcomes
-    outputVector = np.matrix(titanicCSV["Survived"].values).astype(float).T
+    outputVector = np.matrix(titanic_dataset["Survived"].values).astype(float).T
     #Create training, testing subsets
     trainingFeatureMatrix,trainingOutputVector,testingFeatureMatrix,testingOutputVector = utilities.create_TrainingTesting_subsets(featureMatrix,outputVector,0.95)
 
@@ -75,5 +72,5 @@ if REGRESSION_MODE:
 
     #Write predictions from model to excel file and add predictions to the dataframe
     predictions = model.predict(featureMatrix)
-    titanicCSV['Predictions'] = predictions
-    titanicCSV.to_excel('./predictions.xlsx')
+    titanic_dataset['Predictions'] = predictions
+    titanic_dataset.to_excel('./predictions.xlsx')
